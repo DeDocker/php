9a08105aa931380cd2c2b2f24f2acd8bece9845a
Piotr Duszyński <dooshek@desmart.com>
2022-09-19 12:48:58 +0200

    chore: moved more thing to base image, new docker-entrypoitn.sh

--- 7.4/fpm-nginx/ubuntu/create-docker-images.sh
+++ 7.4/fpm-nginx/ubuntu/create-docker-images.sh
@@ -7,4 +7,4 @@ IMAGE="${BASEIMAGE}:${TAG}"
 docker run --privileged --rm tonistiigi/binfmt --install all
 docker buildx create --use --name multiarch
 docker buildx inspect --bootstrap
-docker buildx build --no-cache --pull --platform="linux/amd64,linux/arm64" --tag ${IMAGE} --push .
+docker buildx build --pull --platform="linux/amd64,linux/arm64" --tag ${IMAGE} --push .
--- 7.4/fpm-nginx/ubuntu/docker-entrypoint.sh
+++ 7.4/fpm-nginx/ubuntu/docker-entrypoint.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
-# Start nginx
-/usr/sbin/nginx
+# Load all .env variables into environment variables (needed for PHP to read variables like OPCACHE_VALIDATE_TIMESTAMPS)
+if [ "$CI_ENV" != "testing" ]; then
+    eval $(awk '{print "export " $0}' .env)
+fi
 
-# Start php-fpm
-/usr/sbin/php-fpm7.4
+# Start nginx (only if running user is the same as in docker/Dockerfile)
+if [ "$(id -u)" = 33 ]; then
+    /usr/sbin/nginx
+fi
+
+if [ "$APP_ENV" != "local" ]; then
+    php artisan config:cache
+    php artisan route:cache
+    php artisan view:cache
+    php artisan event:cache
+fi
+exec "$@"
