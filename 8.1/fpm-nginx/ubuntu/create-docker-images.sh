667ed79e0d868120057c1f2478f58153796f763f
Piotr Duszyński <dooshek@desmart.com>
2023-04-21 11:39:23 +0200

    chore: php 8.1

--- 8.1/fpm-nginx/ubuntu/create-docker-images.sh
+++ 8.1/fpm-nginx/ubuntu/create-docker-images.sh
@@ -7,4 +7,5 @@ IMAGE="${BASEIMAGE}:${TAG}"
 docker run --privileged --rm tonistiigi/binfmt --install all
 docker buildx create --use --name multiarch
 docker buildx inspect --bootstrap
-docker buildx build --no-cache --pull --platform="linux/amd64,linux/arm64" --tag ${IMAGE} --push .
+#docker buildx build --no-cache --pull --platform="linux/amd64,linux/arm64" --tag ${IMAGE} --push .
+docker buildx build --no-cache --pull --platform="linux/amd64,linux/arm64" --tag ${IMAGE} $@ .
